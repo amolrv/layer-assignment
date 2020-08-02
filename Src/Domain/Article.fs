f57c1e73059ab7cf2e7453d20bad42d577e4197a
amolrv <amol.vyawhare@outlook.com>
2020-08-03 01:14:29 +0200

    add handling logic of update content and assign to copy writer

--- Src/Domain/Article.fs
+++ Src/Domain/Article.fs
@@ -1,51 +1,95 @@
-namespace Domain
+module Domain.Article
+
+open Domain.Projection
+
+type ArticleState =
+  | InDraft
+  | InReview
+  | Published
+
+type ArticleDraft =
+  { Title : string
+    Content : string
+    CreatorId : JournalistId }
 
 type Article =
   { Title : string
     Content : string
-    OwnerId : JournalistId }
+    OwnerId : JournalistId
+    State : ArticleState
+    Reviewer : CopyWriterId option
+    Comments : Map<CommentId, Comment> }
 
-[<RequireQualifiedAccess>]
-module Article =
-  type ArticleState =
-    private
-    | Draft of Article
-    | InReview of {| Article : Article
-                     Reviewer : CopyWriterId
-                     Comments : Map<CommentId, Comment> |}
-    | Published of {| Article : Article
-                      Reviewer : CopyWriterId |}
+let create (draft : ArticleDraft) : Article =
+  { Title = draft.Title
+    Content = draft.Content
+    OwnerId = draft.CreatorId
+    State = InDraft
+    Reviewer = None
+    Comments = Map.empty }
 
-  type Command =
-    | Draft of Article
-    | ChangeContent of string * string
-    | AssignReviewer of CopyWriterId
-    | Comment of CopyWriterId * Comment * CommentId
-    | ResolveComment of CopyWriterId * CommentId
-    | Publish of JournalistId
+type Command =
+  | Draft of ArticleDraft
+  | ChangeContent of ArticleDraft
+  | AssignReviewer of CopyWriterId
+// | Comment of CopyWriterId * Comment * CommentId
+// | ResolveComment of CopyWriterId * CommentId
+// | Publish of JournalistId
 
-  type Event =
-    | Drafted of Article
-    | AlreadyDrafted
-    | ContentUpdated of string * string
-    | Assigned of CopyWriterId
-    | AlreadyAssigned of CopyWriterId
-    | Commented of CopyWriterId * Comment * CommentId
-    | CommentAlreadyExist of CommentId
-    | CommentResolved of CopyWriterId * CommentId
-    | CommentAlreadyResolved of CommentId
-    | ArticlePublished
-    | ArticleAlreadyPublished
 
-  let apply state event =
-    match event with
-    | Drafted article -> article |> ArticleState.Draft |> Some
-    | _ -> state
+type Event =
+  | ArticleWasNotPresent
+  | Drafted of ArticleDraft
+  | AlreadyDrafted
+  | ContentUpdated of string * string
+  | TriedToChangeContentsOfOthersArticle of ArticleDraft
+  | Assigned of CopyWriterId
+  | AlreadyAssigned of CopyWriterId
+// | Commented of CopyWriterId * Comment * CommentId
+// | CommentAlreadyExist of CommentId
+// | CommentResolved of CopyWriterId * CommentId
+// | CommentAlreadyResolved of CommentId
+// | ArticlePublished
+// | ArticleAlreadyPublished
 
-  let exec cmd state =
-    match cmd, state with
-    | (Draft article, None) -> [ Drafted article ]
-    | (Draft _, _) -> [ AlreadyDrafted ]
-    | _ -> []
+let apply (article : Article option) event =
+  match (event, article) with
+  | (Drafted draft, None) -> draft |> create |> Some
+  | (ContentUpdated (title, content), Some article) ->
+      { article with
+          Title = title
+          Content = content }
+      |> Some
+  | (Assigned reviewer, Some article) ->
+      { article with
+          Reviewer = Some reviewer
+          State = InReview }
+      |> Some
+  | _ -> article
 
-  let articleProjection = { Zero = None ; Fold = apply }
+let private changeContent (draft : ArticleDraft) article =
+  if article.Title = draft.Title
+     && article.Content = draft.Content then
+    []
+  else if draft.CreatorId <> article.OwnerId then
+    [ TriedToChangeContentsOfOthersArticle draft ]
+  else
+    [ ContentUpdated(draft.Title, draft.Content) ]
+
+let private assignReviewer reviewer article =
+  match article.State with
+  | InDraft -> [ Assigned reviewer ]
+  | _ -> [ article.Reviewer |> Option.get |> AlreadyAssigned ]
+
+let exec cmd article =
+  match (cmd, article) with
+  | (Draft articleDraft, None) -> [ Drafted articleDraft ]
+  | (Draft _, Some _) -> [ AlreadyDrafted ]
+  | (ChangeContent draft, Some state) -> state |> changeContent draft
+  | (AssignReviewer reviewer, Some state) -> state |> assignReviewer reviewer
+  | (_, None) -> [ ArticleWasNotPresent ]
+
+let articleProjection = { Zero = None ; Fold = apply }
+
+let articleEventProducer cmd : EventProducer<Event> =
+  (project articleProjection) >> (exec cmd)
--- Test/Domain/ArticleTest.fs
+++ Test/Domain/ArticleTest.fs
@@ -1,27 +1,91 @@
-module Domain.``Article should``
+module Domain.Article
 
-open Xunit
+open Domain.Article
 open FsUnit.Xunit
-open Domain
-open Domain.Projection
+open Xunit
 
-let Given = project Article.articleProjection
-let When = Article.exec
+let Given = id
+let When = articleEventProducer
 let Then expectation actual = actual |> expectation
+
+let empty a = Empty.Matches(a) |> should be True
 
 let article =
   { Title = "Title#1"
     Content = "Some nice content"
-    OwnerId = JournalistId.NewGuid() }
+    CreatorId = JournalistId.NewGuid() }
 
-[<Fact>]
-let ``draft from drafted event`` () =
-  Given []
-  |> When(Article.Command.Draft article)
-  |> Then should equal [ article |> Article.Event.Drafted ]
+let change =
+  { Title = "new title"
+    Content = "new fancy content"
+    CreatorId = article.CreatorId }
 
-[<Fact>]
-let ``emit already drafted event when drafted twice`` () =
-  Given [ article |> Article.Event.Drafted ]
-  |> When(article |> Article.Command.Draft)
-  |> Then should equal [ Article.Event.AlreadyDrafted ]
+let copywriter1 = CopyWriterId.NewGuid()
+let copywriter2 = CopyWriterId.NewGuid()
+
+module DraftArticle =
+  [<Fact>]
+  let ``should draft from drafted event`` () =
+    Given []
+    |> When(Draft article)
+    |> Then should equal [ Drafted article ]
+
+  [<Fact>]
+  let ``should emit already drafted event when article is drafted already`` () =
+    Given [ Drafted article ]
+    |> When(Draft article)
+    |> Then should equal [ AlreadyDrafted ]
+
+module ChangeContent =
+  [<Fact>]
+  let ``should return article was not present`` () =
+    Given []
+    |> When(ChangeContent change)
+    |> Then should equal [ ArticleWasNotPresent ]
+
+  [<Fact>]
+  let ``should update content when article is inDraft state`` () =
+    Given [ Drafted article ]
+    |> When(ChangeContent change)
+    |> Then should equal [ ContentUpdated("new title", "new fancy content") ]
+
+  [<Fact>]
+  let ``should not change content of an article from another journalist`` () =
+    let draftFromOtherJournalist =
+      { change with
+          CreatorId = JournalistId.NewGuid() }
+
+    Given [ Drafted article ]
+    |> When(ChangeContent draftFromOtherJournalist)
+    |> Then should equal [ TriedToChangeContentsOfOthersArticle draftFromOtherJournalist ]
+
+  [<Fact>]
+  let ``should not update content when content are exactly same`` () =
+    Given
+      [ Drafted article
+        ContentUpdated("new title", "new fancy content") ]
+    |> When(ChangeContent change)
+    |> Then be empty
+
+  [<Fact>]
+  let ``should update content when article is in Review state`` () =
+    Given
+      [ Drafted article
+        Assigned copywriter1 ]
+    |> When(ChangeContent change)
+    |> Then should equal [ ContentUpdated("new title", "new fancy content") ]
+
+module Reviewer =
+  [<Fact>]
+  let ``should assign reviewer to article`` () =
+    Given [ Drafted article ]
+    |> When(AssignReviewer copywriter1)
+    |> Then should equal [ Assigned copywriter1 ]
+
+  [<Fact>]
+  let ``should not re assign another reviewer`` () =
+    Given
+      [ Drafted article
+        Assigned copywriter1 ]
+    |> When(AssignReviewer copywriter2)
+    |> Then should equal [ AlreadyAssigned copywriter1 ]
